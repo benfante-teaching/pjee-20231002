8d5e5bda7ef820a59e9a8b145c678624034bb9e3
Lucio Benfante <lucio@benfante.com>
2023-10-04 13:22:16 +0200

    Added redirect and session use

--- demo/src/main/java/com/examples/servlet/HelloServlet.java
+++ demo/src/main/java/com/examples/servlet/HelloServlet.java
@@ -1,24 +1,35 @@
 package com.examples.servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 public class HelloServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        
-        String name = "Lucio";
-        req.setAttribute("name", name);
-
+        HttpSession session = req.getSession();
+        String name = (String)session.getAttribute("name");
+        if (name == null) {
+            req.setAttribute("name", "User (da get)");
+        }
         RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/jsp/hello.jsp");
         dispatcher.forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = req.getParameter("name");
+        if (name == null || name.isBlank()) {
+            name = "User";
+        }
+        HttpSession session = req.getSession();
+        session.setAttribute("name", name);
+        resp.sendRedirect("/demo/hello");
     }
     
 
--- demo/src/main/webapp/WEB-INF/jsp/hello.jsp
+++ demo/src/main/webapp/WEB-INF/jsp/hello.jsp
@@ -7,5 +7,12 @@
 </head>
 <body>
     <h1>Hello ${name}!!</h1>
+    <div>
+        <form action="/demo/hello" method="POST">
+            <label>Name: </label>
+            <input name="name" type="text">
+            <input type="submit" value="Salutami">
+        </form>
+    </div>
 </body>
 </html>
